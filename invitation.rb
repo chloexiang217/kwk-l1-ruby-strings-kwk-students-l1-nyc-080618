6920b72353d109e361d855d44b9991c6d4ddedad
chloexiang217 <chloe.xiang217@gmail.com>
2018-08-06 19:41:26 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,5 +1,4 @@
-puts "Hi, you've been invited to a party, 
-puts "Who are you inviting?"
+puts "Hi, you've been invited to a party! What is your name?"
 guest_name = gets.chomp
 puts "What is the name of the party?"
 party_name = gets.chomp
