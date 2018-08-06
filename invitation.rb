55f92f78ec4b3ffadc9ec2c1ec357c03151dba38
chloexiang217 <chloe.xiang217@gmail.com>
2018-08-06 19:34:01 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,3 +1,6 @@
-puts "w# Code your prompts here!
+guest_name = gets.chomp
+party_name = "Best Halloween Party Ever"
+date = "October 31"
+time = "6 pm"
+host_name = "Harry Potter"
 
-# Try starting out with puts'ing a string.
