cbc100e42ccde5a13d946c8174e05f3562e31f6a
chloexiang217 <chloe.xiang217@gmail.com>
2018-08-06 19:37:11 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,6 +1,11 @@
 guest_name = gets.chomp
-party_name = "Best Halloween Party Ever"
-date = "October 31"
-time = "6 pm"
-host_name = "Harry Potter"
+party_name = gets.chomp
+date = gets.chomp
+time = gets.chomp
+rsvp = gets.chomp
+host_name = gets.chomp 
 
+puts "Dear #{guest_name},
+You are cordially invited to the #{party_name} on #{date} at #{time}. Please RSVP no later than #{rsvp}. 
+Sincerely,
+#{host_name}"
